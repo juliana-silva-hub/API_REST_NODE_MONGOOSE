3a69382585da58c50e154616e8adf17963dde02d
Juliana Cristina da Silva <76570640+juliana-silva-hub@users.noreply.github.com>
2022-05-31 16:11:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // config inicial
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 // forma de ler JSON/ middlewares
@@ -22,4 +23,15 @@ app.get('/', (req, res) => {
 
 
 // entregar uma porta
-app.listen(3000)
+const DB_USER = 'Juliana'
+const DB_PASSWORD = encodeURIComponent('HTM3CJjpTMUsnXt')
+
+mongoose.connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.xzuhg.mongodb.net/bancodaapi?retryWrites=true&w=majority`,
+    )
+    .then(() => {
+        console.log("Conectamos ao MongoDB!")
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
+
