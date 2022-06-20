6b46134884b955b9dff94e20ecace305bb9b088c
Juliana Cristina da Silva <76570640+juliana-silva-hub@users.noreply.github.com>
2022-06-20 16:38:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,37 @@ app.use(
 
 app.use(express.json())
 
+// rotas da API
+app.post('/person', async (req, res) => {
+   
+   //req.body
+
+   // {name: "Juliana", salary: 5000, approved: false}
+   const {name, salary, approved} = req.body
+
+   const person = {
+     name,
+     salary,
+     approved
+   }
+
+   try {
+
+     // criando dados
+     await Person.create(person)
+
+     res.status(201). json({message: 'Pessoa inserida no sistema com sucesso!'})
+   } catch (error) {
+     res.status(500). json({error: error})
+   }
+
+})
+ 
 // rota inicial / endpoint
 app.get('/', (req, res) => {
 
 
-    // mostrar req
+// mostrar req
 
     res.json({message: 'Oi Express!' })
 })
